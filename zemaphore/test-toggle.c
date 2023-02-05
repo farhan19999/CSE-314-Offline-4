61be588712387179562d476afeadcd93ae91fd3a
farhan19999 <48511298+farhan19999@users.noreply.github.com>
2023-02-05 20:28:18 +0600

    Revert "zem done and gitignore update"

--- zemaphore/test-toggle.c
+++ zemaphore/test-toggle.c
@@ -6,24 +6,18 @@
 #include <signal.h>
 #include <wait.h>
 #include <pthread.h>
-#include "zemaphore.h"
 
 #define NUM_THREADS 3
 #define NUM_ITER 10
-
-zem_t zems[NUM_THREADS];
 
 void *justprint(void *data)
 {
   int thread_id = *((int *)data);
 
   for(int i=0; i < NUM_ITER; i++)
-  {
-    zem_down(&zems[thread_id]);
-    printf("This is thread %d\n", thread_id);
-    zem_up(&zems[(thread_id+1)%NUM_THREADS]);
-  }
-  
+    {
+      printf("This is thread %d\n", thread_id);
+    }
   return 0;
 }
 
@@ -35,18 +29,15 @@ int main(int argc, char *argv[])
 
   
   for(int i =0; i < NUM_THREADS; i++)
-  {
-    zem_init(&zems[i],0);
-    mythread_id[i] = i;
-    pthread_create(&mythreads[i], NULL, justprint, (void *)&mythread_id[i]);
-  }
-
-  zem_up(&zems[0]);
+    {
+      mythread_id[i] = i;
+      pthread_create(&mythreads[i], NULL, justprint, (void *)&mythread_id[i]);
+    }
   
   for(int i =0; i < NUM_THREADS; i++)
-  {
-    pthread_join(mythreads[i], NULL);
-  }
+    {
+      pthread_join(mythreads[i], NULL);
+    }
   
   return 0;
 }
